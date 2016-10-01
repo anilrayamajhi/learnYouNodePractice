5ff57103134cf6c9b78aa4a7714d58742bd455d8
Anil Rayamajhi <anilrm07@gmail.com>
2016-10-01 13:22:58 -0700

    Done till EX5

---
+++ EX5.js
@@ -0,0 +1,5 @@
+var fs = require('fs'), path = require('path'), ext = "." +process.argv[3];
+
+fs.readdir(process.argv[2], ext , function (err, list) {
+  list.forEach(function(el) { (path.extname(el) === ext) ?  console.log(el) : null});
+});
